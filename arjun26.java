fa00e66d6465a06f88378aa99104f8ca888aafb4
arjunmudaliyar74 <167169074+arjunmudaliyar74@users.noreply.github.com>
2024-06-26 11:38:22 +0530

    Add files via upload

---
+++ arjun26.java
@@ -0,0 +1,39 @@
+class Break
+{
+	public static void main(String[] args)
+	{
+		boolean t=true;
+		first:{
+		  second:{
+		    third:{
+		       System.out.println("before the break");
+		        if(t)
+			break second;
+			System.out.println("this won't execute");
+		           }
+		         System.out.println("this wont execute");
+		       }
+		     System.out.println("this is after second block");
+                           }	
+	}
+
+}class Break
+{
+	public static void main(String[] args)
+	{
+		boolean t=true;
+		first:{
+		  second:{
+		    third:{
+		       System.out.println("before the break");
+		        if(t)
+			break second;
+			System.out.println("this won't execute");
+		           }
+		         System.out.println("this wont execute");
+		       }
+		     System.out.println("this is after second block");
+                           }	
+	}
+
+}
